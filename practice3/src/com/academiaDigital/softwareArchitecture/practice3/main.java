9ef4080735f41566e898870256ab7f0a4ac1d8f3
AlexFlores-TrainingAssureSoft <alex.flores@training.assuresoft.com>
2022-05-26 15:17:52 -0400

    Practice 3 enhanced

--- practice3/src/com/academiaDigital/softwareArchitecture/practice3/main.java
+++ practice3/src/com/academiaDigital/softwareArchitecture/practice3/main.java
@@ -8,13 +8,15 @@ import com.academiaDigital.softwareArchitecture.practice3.Files.IFIleOperations;
 public class main {
     public static void main(String[] args) {
         DriveCreator creatorCSV = new CSVcreator();
-        IFIleOperations csvObject = creatorCSV.createObject();
-
         creatorCSV.getUsers("c:\\users\\data.csv");
 
-        DriveCreator creatorXML = new XMLcreator();
-        IFIleOperations xmlObject = creatorXML.createObject();
+        IFIleOperations csvObject = creatorCSV.createObject();
+        csvObject.read("c:\\users\\data.csv");
 
-        creatorXML.getUsers("c:\\users\\data.xml");
+        DriveCreator creatorXML = new XMLcreator();
+        creatorCSV.getUsers("c:\\users\\data.xml");
+
+        IFIleOperations xmlObject = creatorXML.createObject();
+        xmlObject.read("c:\\users\\data.xml");
     }
 }
