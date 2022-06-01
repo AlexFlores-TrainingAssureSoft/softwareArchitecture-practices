61358b62d724d6993c75542bf33c8ad951079cfd
AlexFlores-TrainingAssureSoft <alex.flores@training.assuresoft.com>
2022-06-01 00:13:45 -0400

    create DTO mysql and dynamoDB

--- Practice7/src/main/java/com/academiaDigital/softwareArchitecture/UserInformation.java
+++ Practice7/src/main/java/com/academiaDigital/softwareArchitecture/UserInformation.java
@@ -1,12 +1,21 @@
 package com.academiaDigital.softwareArchitecture;
 
-public class UserInformation {
+public class UserInformation2 {
     private String name;
     private String lastName;
+    private String type;
+    private double amount;
 
-    public UserInformation(String name, String lastName) {
+    private MysqlDto mysqlDto;
+    private DynamoDbDto dynamoDbDto;
+
+    public UserInformation2(String name, String lastName, String type, double amount) {
         this.name = name;
         this.lastName = lastName;
+        this.type = type;
+        this.amount = amount;
+        mysqlDto = new MysqlDto(name,lastName);
+        dynamoDbDto = new DynamoDbDto(type,amount);
     }
 
     public String getName() {
@@ -25,4 +34,19 @@ public class UserInformation {
         this.lastName = lastName;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
